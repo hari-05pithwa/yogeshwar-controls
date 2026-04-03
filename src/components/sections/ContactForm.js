aa907b6014ba39129361f7839369c983a234f4d0
Hari Pithwa <haripithwa2005@gmail.com>
2026-04-03 11:17:16 +0530

    contact form spacing 2

--- src/components/sections/ContactForm.js
+++ src/components/sections/ContactForm.js
@@ -117,7 +117,7 @@ export default function ContactForm() {
   };
 
   return (
-    <section ref={sectionRef} className="mb:10 bg-white px-6 relative">
+    <section ref={sectionRef} className="py-20 bg-white px-6 relative">
       
       {/* RESPONSIVE SMOOTH ALERT */}
       {alertMsg.show && (
